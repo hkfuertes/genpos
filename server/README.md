734ee3df16cee917b7f1a6f7e5589918f5492031
Miguel Fuertes <hkfuertes@gmail.com>
2022-07-18 12:59:38 +0200

    Initial Commit

---
+++ server/README.md
@@ -0,0 +1,5 @@
+# Ruby on Rails Server
+To start up the project:
+```bash
+docker-compose up [-d] --build
+```
---
+++ server/app/assets/config/manifest.js
@@ -0,0 +1,5 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link jquery/jquery.min.js
+//= link bootstrap/bootstrap.bundle.min.js
+//= link adminlte/adminlte.min.js
