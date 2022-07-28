160b0c88f646b305adae9024f5682d435710d529
Miguel Fuertes <hkfuertes@gmail.com>
2022-07-28 20:50:25 +0200

    students done ui

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'students', to: 'students#index', name: 'students_home'
-  #get 'home/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "home#index", name: 'home'
+  resources :students
+  root 'home#index', name: 'home'
 end
