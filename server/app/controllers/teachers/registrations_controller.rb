c7a4b54882305cee3f4f74f2dac8f78b7c75eebe
Miguel Fuertes <hkfuertes@gmail.com>
2022-08-03 20:48:34 +0200

    TODO: remove sign_up

--- server/app/controllers/teachers/registrations_controller.rb
+++ server/app/controllers/teachers/registrations_controller.rb
@@ -4,25 +4,20 @@ class Teachers::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
-
   # POST /resource
   # def create
   #   super
   # end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+  end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # DELETE /resource
   # def destroy
@@ -41,9 +36,9 @@ class Teachers::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute], classrooms: [])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
